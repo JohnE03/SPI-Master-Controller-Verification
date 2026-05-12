5897203ac8fae5b27892e6e0afb850825861ef9a
JohnE03 <2101519@eng.asu.edu.eg>
2026-05-12 14:09:56 +0300

    Mina

--- assertions/spi_sva.sv
+++ assertions/spi_sva.sv
@@ -45,15 +45,13 @@ module spi_sva (
     // Since this SVA is bound to u_regfile, we use absolute hierarchical paths to reach u_core.
     wire cpol = tb_top.u_wrap.u_dut.u_core.cfg_mode[1];
     wire cpha = tb_top.u_wrap.u_dut.u_core.cfg_mode[0];
-    wire sclk_rose = $rose(tb_top.u_wrap.u_dut.u_core.SCLK);
-    wire sclk_fell = $fell(tb_top.u_wrap.u_dut.u_core.SCLK);
-    
-    // Calculate if the current PCLK cycle contains a sample edge
-    wire is_sample_edge = cpha ? (cpol ? sclk_rose : sclk_fell) : (cpol ? sclk_fell : sclk_rose);
-
+ 
     a_mosi_stability: assert property (
         @(posedge PCLK) disable iff (!PRESETn || !tb_top.u_wrap.u_dut.u_core.busy)
-            is_sample_edge |-> $stable(tb_top.u_wrap.u_dut.u_core.MOSI)
+            (
+                ((cpha == cpol) && $rose(tb_top.u_wrap.u_dut.u_core.SCLK)) ||
+                ((cpha != cpol) && $fell(tb_top.u_wrap.u_dut.u_core.SCLK))
+            ) |-> $stable(tb_top.u_wrap.u_dut.u_core.MOSI)
     ) else $error("[ASSERTION_ERROR] a_mosi_stability: MOSI changed during sample edge!");
 
 endmodule
